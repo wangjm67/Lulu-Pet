26701b86bd8828aa63bbabe1d9f310943461d57f
wangjm67 <151261045+wangjm67@users.noreply.github.com>
2026-07-22 03:00:15 +0800

    Refactor petData object structure and properties

--- src/core/petData.js
+++ src/core/petData.js
@@ -1,41 +1,37 @@
-export const petData = {
+export const petData={
 
 
-    // 基础信息
-
-    name:"噜噜",
-
-    age:1,
-
-    level:1,
-
-    exp:0,
+name:"噜噜",
 
 
-    // 养成属性
-
-    mood:100,
-
-    hunger:100,
-
-    energy:100,
+age:1,
 
 
-    // 互动数据
-
-    touchCount:0,
-
-    coin:0,
+level:1,
 
 
-    // 当前状态
-
-    state:"idle",
+exp:0,
 
 
-    // 时间记录
+mood:100,
 
-    lastUpdate:Date.now()
+
+hunger:100,
+
+
+energy:100,
+
+
+coin:20,
+
+
+touchCount:0,
+
+
+foodCount:0,
+
+
+lastUpdate:Date.now()
 
 
 }
